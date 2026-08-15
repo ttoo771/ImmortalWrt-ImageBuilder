999623a03f4b428602862505984e4e7f3df8e0ca
ttoo771 <71752234+ttoo771@users.noreply.github.com>
2026-08-15 23:54:05 +0800

    luci-app-openclash

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -65,6 +65,8 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
 # 自定义命令
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-commands-zh-cn"
 
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
+
 #===========================以下imm仓库内的软件==============================↓
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-3cat-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-3ginfo-lite-zh-cn"
